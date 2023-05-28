e2cef5a0e95e81e76699525b21081d3910f62c81
Alexey Derendyaev <alexderendyaev@yandex.ru>
2023-05-28 16:40:44 -0300

    Day 5-2

--- news_app/users/views.py
+++ news_app/users/views.py
@@ -1,10 +1,4 @@
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views import View
 from django.contrib.auth.views import LoginView, LogoutView
-
-from users.forms import UserLoginForm
 
 
 class UserLoginView(LoginView):
